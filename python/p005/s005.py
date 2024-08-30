f793f0f15413805b4158e0d8a25b29182abc61ba
GitAkash <akash@mondal.zip>
2024-08-30 09:19:08 +0200

    Changed some code around, still doesn't work..

--- python/p005/s005.py
+++ python/p005/s005.py
@@ -1,10 +1,9 @@
+n = 1
+
 while True:
-    n = 1
-    
-    if n % 19 == 0 and n % 17 == 0 and n % 13 == 0 and n%11 == 0 and n % 7 == 0 and n % 5 == 0 and n % 3 == 0 and n % 2 == 0:
+    if n % 19 == 0 and n % 17 == 0 and n % 13 == 0 and n % 11 == 0 and n % 7 == 0 and n % 5 == 0 and n % 3 == 0 and n % 15 == 0 and n % 9 == 0 and n % 2 == 0:
         print(n)
         break
         
     else: 
         n += 1
-             
