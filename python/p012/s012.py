738f07e2cde293287b41458dfaebd9bbd2d85f65
GitAkash <akash@mondal.zip>
2024-09-20 16:21:11 +0200

    Tried making it faster, did it by creating a new version

--- python/p012/s012.py
+++ python/p012/s012.py
@@ -8,7 +8,7 @@ Needs a super fast extreme fast computer, this algorithm is like n^3489274
 def triangle(n):
     c = 0
     sumlist = []
-    while c < n:
+    if c > n:
         tempsum = []
         for i in range(c+1):
             tempsum.append(i)
@@ -18,19 +18,23 @@ def triangle(n):
 
 def factorize(n):
     factors = []
-    for i in range(1,n+1):
+    for i in range(1,int((n+1)**0.5)):
         if n % i == 0:
             factors.append(i)
+            if n / i != i:
+                factors.append(n/i)
     return factors 
 
-def factortriangle(n):
+def factortriangle(n, divisors):
     finalfactors = []
     values = triangle(n)
+    #print(values)
     for i in values:
-        finalfactors.append((i, factorize(i), len(factorize(i)))) 
-        if len(factorize(i)) > 500:
-               print(i, len(factorize(i)))
+        factorizednumb = factorize(i)
+        finalfactors.append((i, factorizednumb, len(factorizednumb))) 
+        if len(factorizednumb) > divisors:
+            print(i, len(factorizednumb))
     return (finalfactors)
 
-factortriangle(3000)
+print(factortriangle(1000000000, 5))
 
