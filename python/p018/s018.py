73b9d1c24275f95fdeac90440b1ba71d5d8aba36
GitAkash <akash@mondal.zip>
2024-09-26 22:14:47 +0200

    Lord what am i doing

--- python/p018/s018.py
+++ python/p018/s018.py
@@ -13,7 +13,7 @@ pyramid = """75
 91 71 52 38 17 14 91 43 58 50 27 29 48
 63 66 04 68 89 53 67 30 73 16 69 87 40 31
 04 62 98 27 23 09 70 98 73 93 38 53 60 04 23"""
-
+max = 0
 rows = pyramid.splitlines()
 intpyramid = []
 converted_row = []
@@ -24,10 +24,91 @@ for row in rows:
     intpyramid.append(converted_row)
     converted_row = []
 
-i = 0
-for j in range(len(intpyramid)):
-    i += 1
-    for k in range(len(intpyramid[j])):
-        print(intpyramid[j][i-1])
-    #Choose next row between the i-1, i, i+1
-    #Forgot how to find an index need to look it up again whoops 
+
+# print(intpyramid)
+for row in range(len(intpyramid)):
+    for column in range(len(intpyramid[row])):
+        sequence0 = []
+        sequence1 = []
+        sequence2 = []
+        sequence3 = []
+        sequence4 = []
+        sequence5 = []
+        sequence6 = []
+        sequence7 = []
+        current_number = intpyramid[row][column]
+        if row <= 11:
+        #Possiblities
+        
+            sequence0.append(intpyramid[row+1][column])
+            sequence0.append(intpyramid[row+2][column])
+            sequence0.append(intpyramid[row+3][column])
+
+            sequence1.append(intpyramid[row+1][column+1])
+            sequence1.append(intpyramid[row+2][column])
+            sequence1.append(intpyramid[row+3][column])
+
+            sequence2.append(intpyramid[row+1][column+1])
+            sequence2.append(intpyramid[row+2][column+1])
+            sequence2.append(intpyramid[row+3][column])
+        
+            sequence3.append(intpyramid[row+1][column+1])
+            sequence3.append(intpyramid[row+2][column])
+            sequence3.append(intpyramid[row+3][column+1])
+        
+            sequence4.append(intpyramid[row+1][column])
+            sequence4.append(intpyramid[row+2][column+1])
+            sequence4.append(intpyramid[row+3][column])
+        
+            sequence5.append(intpyramid[row+1][column])
+            sequence5.append(intpyramid[row+2][column+1])
+            sequence5.append(intpyramid[row+3][column+1])
+        
+            sequence6.append(intpyramid[row+1][column])
+            sequence6.append(intpyramid[row+2][column])
+            sequence6.append(intpyramid[row+3][column+1])
+        
+            sequence7.append(intpyramid[row+1][column+1])
+            sequence7.append(intpyramid[row+2][column+1])
+            sequence7.append(intpyramid[row+3][column+1])
+
+        if sum(sequence0) + current_number > max:
+            max = sum(sequence0) + current_number
+
+        if sum(sequence1) + current_number > max:
+            max = sum(sequence1) + current_number
+
+        if sum(sequence2) + current_number > max:
+            max = sum(sequence2) + current_number
+
+        if sum(sequence3) + current_number > max:
+            max = sum(sequence3) + current_number
+
+        if sum(sequence4) + current_number > max:
+            max = sum(sequence4) + current_number
+
+        if sum(sequence5) + current_number > max:
+            max = sum(sequence5) + current_number
+        
+        if sum(sequence6) + current_number > max:
+            max = sum(sequence6) + current_number
+        
+        if sum(sequence7) + current_number > max:
+            max = sum(sequence7) + current_number
+
+        print(sequence0, sequence1, sequence2, sequence3, sequence4, sequence5, sequence6, sequence7)
+        #Values that are connected to the value above.
+        # if row <= 13:
+            # next_number0 = intpyramid[row+1][column]
+            # next_number1 = intpyramid[row+1][column+1]
+        # if row <= 11:
+            # next_list_l = [intpyramid[row+i][column] for i in range(4)]
+            # if column < len(intpyramid[row])-1:
+                # next_list_r = [intpyramid[row+i][column+1]for i in range(4)]
+                # print(next_list_l, next_list_r)
+
+        #Check if value is the last row.
+        # else:
+            # next_number0 = 0
+            # next_number1 = 0
+        # print(next_number0)    
